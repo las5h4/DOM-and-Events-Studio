879618bbee138444f48d74a0b2c1c59efdceb156
las5h4 <loganasmalley@gmail.com>
2019-12-03 15:31:30 -0600

    added functionality to land button - shows alert dialog, changes flight status, changes bg color, changes height

--- scripts.js
+++ scripts.js
@@ -4,11 +4,20 @@ window.addEventListener("load", function(){
     let takeoff = document.getElementById("takeoff")
     let flightStatus = document.getElementById("flightStatus")
     let shuttleBackground = document.getElementById("shuttleBackground")
+    let shuttleHeight = document.getElementById("spaceShuttleHeight")
+    let land = document.getElementById("landing")
     takeoff.addEventListener("click", function (){
         let confirmation = window.confirm("Confirm that the shuttle is ready for takeoff.")
         if (confirmation === true) {
             flightStatus.innerHTML = "Shuttle in flight"
             shuttleBackground.style.backgroundColor = "blue"
+            shuttleHeight.innerHTML = 10000
         }
+    })
+    land.addEventListener("click", function (){
+        window.alert("Shuttle is landing. Langing gear engaged.")
+        flightStatus.innerHTML = "Shuttle has landed"
+        shuttleBackground.style.backgroundColor = "green"
+        shuttleHeight.innerHTML = 0
     })
 })
