004fb81461e347a86c417ccd0517456c3cf830b9
las5h4 <loganasmalley@gmail.com>
2019-12-03 17:04:08 -0600

    wrote 'change height' function that returns an object with the new shuttle height and moves the rocket vertically

--- scripts.js
+++ scripts.js
@@ -1,18 +1,40 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function changeHeight(direction, height) {
+    if (direction === "up") {
+        height += 10000
+    } else if (direction === "down") {
+        height -= 10000
+    }
+    let imgHeight = height / 1000
+    let heightObj = {
+        height: height,
+        imgHeight: imgHeight
+    }
+    return heightObj
+}
 window.addEventListener("load", function(){
     let takeoff = document.getElementById("takeoff")
     let flightStatus = document.getElementById("flightStatus")
     let shuttleBackground = document.getElementById("shuttleBackground")
     let shuttleHeight = document.getElementById("spaceShuttleHeight")
+    shuttleHeightNum = Number(shuttleHeight.innerHTML)
     let land = document.getElementById("landing")
     let missionAbort = document.getElementById("missionAbort")
+    let up = document.getElementById("up")
+    let down = document.getElementById("down")
+    let right = document.getElementById("right")
+    let left = document.getElementById("left")
+    let rocket = document.querySelector("img")    
     takeoff.addEventListener("click", function (){
         let confirmation = window.confirm("Confirm that the shuttle is ready for takeoff.")
         if (confirmation === true) {
             flightStatus.innerHTML = "Shuttle in flight"
             shuttleBackground.style.backgroundColor = "blue"
-            shuttleHeight.innerHTML = 10000
+            let newHeightObj = changeHeight("up", shuttleHeightNum)            
+            shuttleHeightNum = newHeightObj.height
+            shuttleHeight.innerHTML = shuttleHeightNum
+            rocket.style.bottom = String(newHeightObj.imgHeight)+"px"
         }
     })
     land.addEventListener("click", function (){
@@ -28,5 +50,12 @@ window.addEventListener("load", function(){
             shuttleBackground.style.backgroundColor = "green"
             shuttleHeight.innerHTML = 0
         }
+    })
+    
+    up.addEventListener("click", function(){
+        let newHeightObj = changeHeight("up", shuttleHeightNum)            
+        shuttleHeightNum = newHeightObj.height
+        shuttleHeight.innerHTML = shuttleHeightNum
+        rocket.style.bottom = String(newHeightObj.imgHeight)+"px"
     })
 })
