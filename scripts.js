ca55f57ca57a0958f8bcd234f89591aecf1374ce
las5h4 <loganasmalley@gmail.com>
2019-12-03 15:16:55 -0600

    added functionality to takeoff button - changes the background color to blue

--- scripts.js
+++ scripts.js
@@ -3,10 +3,12 @@
 window.addEventListener("load", function(){
     let takeoff = document.getElementById("takeoff")
     let flightStatus = document.getElementById("flightStatus")
+    let shuttleBackground = document.getElementById("shuttleBackground")
     takeoff.addEventListener("click", function (){
         let confirmation = window.confirm("Confirm that the shuttle is ready for takeoff.")
         if (confirmation === true) {
             flightStatus.innerHTML = "Shuttle in flight"
+            shuttleBackground.style.backgroundColor = "blue"
         }
     })
 })
